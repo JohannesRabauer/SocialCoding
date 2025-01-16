a6684a57b399ce0f7b22e4f0674c7688a4e2c464
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-01-16 20:03:12 +0100

    Init

---
+++ src/main/java/de/johannes_rabauer/copilot_test/CopilotTestApplication.java
@@ -0,0 +1,13 @@
+package de.johannes_rabauer.copilot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotTestApplication.class, args);
+	}
+
+}
